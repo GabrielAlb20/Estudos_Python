e039136d4fabbe29d9e8abc2a4883a7865e44f1c
GabrielAlbqr30 <gabrielg@petribusa.com.br>
2025-05-13 16:57:48 -0300

    ✨ feat: adicionando introdução do POO em Python

---
+++ EstudosPython/14_orientacao_objetos/01_o_que_eh.py
@@ -0,0 +1,14 @@
+"""
+Orientação a Objetos o que é
+
+Paradigmas de programação 
+-------------------------------------
+Programação Estruturada             /
+Programação Orientada a Objetos     /
+Programação Funcional               /
+------------------------------------
+
+como vou pensar o meu software?
+como vou desenvolver?
+
+"""
