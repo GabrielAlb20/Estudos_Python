b139a92b696c4700065ffd6c0603e381b2f0a6f2
GabrielAlbqr30 <gabrielg@petribusa.com.br>
2025-05-16 10:28:36 -0300

    🏋️‍♂️ exercise: adicionando/finalizando os  exercícios da sessão:  POO

--- EstudosPython/14_orientacao_objetos/exercicios/exercicio01.py
+++ EstudosPython/14_orientacao_objetos/exercicios/exercicio01.py
@@ -1,53 +1,64 @@
-''''
-
-1. Crie uma classe Pessoa, contendo nome, data de nascimento e email. Crie as propriedades getters e
-setters para os atributos e um método para imprimir os dados de uma pessoa.
-
-'''
-
 from datetime import date
 
 
 class Pessoa:
+    """
+    Esta classe representa uma pessoa com nome, data de nascimento e email.
+    """
 
     def __init__(self, nome: str, data_nascimento: date, email: str) -> None:
-        self.__nome: str = nome
-        self.__data_nascimento: date = data_nascimento
-        self.__email: str = email
+        """
+        Inicializa um objeto Pessoa.
+
+        Args:
+            nome (str): O nome da pessoa.
+            data_nascimento (date): A data de nascimento da pessoa.
+            email (str): O email da pessoa.
+        """
+        self.__nome = nome
+        self.__data_nascimento = data_nascimento
+        self.__email = email
 
     @property
     def nome(self) -> str:
+        """Obtém o nome da pessoa."""
         return self.__nome
-    
+
     @nome.setter
-    def nome(self, nome:str) -> None:
+    def nome(self, nome: str) -> None:
+        """Define o nome da pessoa."""
         self.__nome = nome
-    
+
     @property
-    def data_nascimento(self) -> str:
+    def data_nascimento(self) -> date:
+        """Obtém a data de nascimento da pessoa."""
         return self.__data_nascimento
-    
+
     @data_nascimento.setter
     def data_nascimento(self, data_nascimento: date) -> None:
+        """Define a data de nascimento da pessoa."""
         self.__data_nascimento = data_nascimento
 
-    
     @property
     def email(self) -> str:
+        """Obtém o email da pessoa."""
         return self.__email
-    
+
     @email.setter
-    def email(self, email:str) -> None:
+    def email(self, email: str) -> None:
+        """Define o email da pessoa."""
         self.__email = email
 
-    
     def imprimir(self) -> None:
+        """Imprime os dados da pessoa formatados."""
         print(f'Nome: {self.__nome}')
-        print(f'E-mail: {self.email}')
-        print(f'Data Nascimento: {self.data_nascimento.strtime('%d/%m/%Y')}')
+        print(f'E-mail: {self.__email}')
+        print(f'Data Nascimento: {self.__data_nascimento.strftime("%d/%m/%Y")}')
 
 
 if __name__ == '__main__':
-    p: Pessoa = Pessoa(nome:'Felicity Jones', data(year:1987, mont:7, day:22), email:'felicity@gmail.com')
+    # Criando um objeto Pessoa
+    p = Pessoa(nome='Felicity Jones', data_nascimento=date(year=1987, month=7, day=22), email='felicity@gmail.com')
 
+    # Imprimindo os dados da pessoa
     p.imprimir()
