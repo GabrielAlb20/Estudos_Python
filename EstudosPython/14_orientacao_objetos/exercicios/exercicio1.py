af7728a736d61f3ce5cf8c0eda06d203e1b95326
GabrielAlbq20 <gabriel.albqdev@outlook.com>
2025-05-15 21:55:44 -0300

    🏋️‍♂️ exercise: adicionando a outra versão do exercício 1 sessão de POO.

--- EstudosPython/14_orientacao_objetos/exercicios/exercicio1.py
+++ EstudosPython/14_orientacao_objetos/exercicios/exercicio1.py
@@ -3,11 +3,6 @@
 setters para os atributos e um método para imprimir os dados de uma pessoa.
 
 """
-"""
-1. Crie uma classe Pessoa, contendo nome, data de nascimento e email. Crie as propriedades getters e
-setters para os atributos e um método para imprimir os dados de uma pessoa.
-
-"""
 # MINHA VERSÃO, ADICIONEI MAIS DETALHES SOBRE A 'PESSOA'
 class Pessoa:
     
