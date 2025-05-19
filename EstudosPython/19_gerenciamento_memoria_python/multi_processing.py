d3cb6fd4d84eeb4490f572b3919006a3ab92bba0
GabrielAlbqr30 <gabrielg@petribusa.com.br>
2025-05-19 14:53:03 -0300

    ✨ feat: adicionando arquivos da sessão Gerenciamento de memória em Python.

---
+++ EstudosPython/19_gerenciamento_memoria_python/multi_processing.py
@@ -0,0 +1,21 @@
+
+import time
+from multiprocessing import Pool
+
+CONTADOR = 50000000
+
+def contagem_regressiva(n):
+    while n > 0:
+        n -= 1
+
+if __name__ == '__main__':
+    pool = Pool(processes=2)
+    inicio = time.time()
+    r1 = pool.apply_async(contagem_regressiva, [CONTADOR//2])
+    r2 = pool.apply_async(contagem_regressiva, [CONTADOR//2])
+    pool.close()
+    pool.join()
+    fim = time.time()
+    print(f'Tempo em segundos: {fim - inicio}')
+    
+ # Tempo em segundos: 0.4931497573852539 - Mais performance
